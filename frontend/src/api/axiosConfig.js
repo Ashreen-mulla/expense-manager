5f387c865d3872c8381f6cc19df69ceee61c6a29
Ashreen Mulla <ashreensk786@gmail.com>
2026-07-11 20:00:49 +0530

    chore: configure production backend URL

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,20 +1,3 @@
-import axios from "axios";
-
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
-    headers: {
-        "Content-Type": "application/json",
-    },
+    baseURL: "https://expense-manager-uzl1.onrender.com/api/v1",
 });
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-});
-
-export default api;
